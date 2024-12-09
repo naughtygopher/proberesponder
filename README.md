ca17bb3b7399b4e8c0564cde37566a20208661c0
Kamaleshwar <bn_kamalesh@yahoo.com>
2024-12-09 15:39:12 +0100

    [minor] added HTTP handlers for convenience, to setup HTTP server for responding to probes

--- README.md
+++ README.md
@@ -3,8 +3,26 @@
 [![Go Report Card](https://goreportcard.com/badge/github.com/naughtygopher/proberesponder?cache_invalidate=v0.3.0)](https://goreportcard.com/report/github.com/naughtygopher/proberesponder)
 [![Coverage Status](https://coveralls.io/repos/github/naughtygopher/proberesponder/badge.svg?branch=main&cache_invalidate=v0.3.0)](https://coveralls.io/github/naughtygopher/proberesponder?branch=main)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/creativecreature/sturdyc/blob/master/LICENSE)
-[![Mentioned in Awesome Go](https://awesome.re/mentioned-badge.svg)](https://github.com/avelino/awesome-go?tab=readme-ov-file#caches)
 
 # Proberesponder
 
-Probe-responder is a package to deal with handling [appropriate statuses for Kuberentes](https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/).
+Probe-responder is a package to deal with handling [appropriate statuses for Kuberentes](https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/). Even though the statuses are specific to Kubernetes, it can be used in any context, and can later be updated to include more if required.
+
+# Sample usage
+
+```golang
+package main
+import (
+    "github.com/naughtygopher/proberesponder"
+)
+
+func main() {
+    pRes := proberesponder.New()
+    // do something to startup your app, once finished
+    pRes.SetNotStarted(false)
+
+    // if you're running an API server (HTTP, gRPC etc.), start the servers and then.
+    pRes.SetNotLive(false)
+	pRes.SetNotReady(false)
+}
+```
--- http.go
+++ http.go
@@ -1,1 +1,99 @@
 package proberesponder
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const (
+	httpHeaderContentType      = "Content-Type"
+	httpHeaderContentTypeJSON  = "application/json"
+	httpHeaderContentTypeHTML  = "text/html"
+	httpHeaderContentTypePlain = "text/plain"
+)
+
+func HTTPStartup(pres *ProbeResponder) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		status := http.StatusOK
+		if pres.NotStarted() {
+			status = http.StatusServiceUnavailable
+		}
+		w.WriteHeader(status)
+		respond(pres, w, r)
+	}
+}
+
+func HTTPReady(pres *ProbeResponder) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		status := http.StatusOK
+		if pres.NotReady() {
+			status = http.StatusServiceUnavailable
+		}
+		w.WriteHeader(status)
+		respond(pres, w, r)
+	}
+}
+
+func HTTPLive(pres *ProbeResponder) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		status := http.StatusOK
+		if pres.NotLive() {
+			status = http.StatusServiceUnavailable
+		}
+		w.WriteHeader(status)
+		respond(pres, w, r)
+	}
+}
+
+func respond(pres *ProbeResponder, w http.ResponseWriter, r *http.Request) {
+	contentType, bPayload := contentNeogiater(r, pres.HealthResponse())
+	w.Header().Add(httpHeaderContentType, contentType)
+	_, err := w.Write(bPayload)
+	if err != nil {
+		log.Println("failed to write response", err)
+	}
+}
+
+func contentNeogiater(r *http.Request, payload map[string]string) (cType string, bPayload []byte) {
+	ctype := r.Header.Get(httpHeaderContentType)
+	if strings.Contains(ctype, httpHeaderContentTypeHTML) {
+		cType = httpHeaderContentTypeHTML
+		bPayload = responseAsHTML(payload)
+	} else if strings.Contains(ctype, httpHeaderContentTypePlain) {
+		cType = httpHeaderContentTypePlain
+		bPayload = responseAsPlainText(payload)
+	} else {
+		// default is json
+		cType = httpHeaderContentTypeJSON
+		bPayload, _ = json.Marshal(payload)
+	}
+
+	return cType, bPayload
+}
+
+func responseAsHTML(payload map[string]string) []byte {
+	buff := bytes.NewBufferString(`<table><tbody>`)
+	for key, value := range payload {
+		buff.WriteString(`<tr>` +
+			`<th>` + key + `</th>` +
+			`<td>` + value + `</td>` +
+			`</tr>`)
+	}
+	buff.WriteString(`</tbody></table>`)
+	return buff.Bytes()
+}
+
+func responseAsPlainText(payload map[string]string) []byte {
+	buff := bytes.NewBuffer([]byte{})
+	for key, value := range payload {
+		buff.WriteString(fmt.Sprintf("%s: %s|", key, value))
+	}
+	return buff.Bytes()
+}
